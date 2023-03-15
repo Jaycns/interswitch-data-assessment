dc74109383cffc5820f675ef376a37dbd69b81e3
Jude Okonkwo <80961031+Jaycns@users.noreply.github.com>
2023-03-15 16:56:49 +0100

    Finished work
    Complete solution of Data Engineering Assessment

---
+++ map.py
@@ -0,0 +1,125 @@
+outcome_map = {
+  "001": "Fully complete CATI interview",
+  "002": "Partially completed CATI interview",
+  "003": "Complete but personal visit requested next month",
+  "004": "Partial, not complete at closeout",
+  "005": "Labor force complete, supplement incomplete - CATI",
+  "006": "LF complete, supplement DK items incomplete at closeout–ASEC only",
+  "020": "HH occupied entirely by Armed Forces members or all under 15 years of age",
+  "201": "CAPI complete",
+  "202": "Callback needed",
+  "203": "Sufficient partial - precloseout",
+  "204": "Sufficient partial - at closeout",
+  "205": "Labor force complete, - suppl. incomplete - CAPI",
+  "213": "Language barrier",
+  "214": "Unable to locate",
+  "216": "No one home",
+  "217": "Temporarily absent",
+  "218": "Refused",
+  "219": "Other occupied - specify",
+  "223": "Entire household Armed Forces",
+  "224": "Entire household under 15",
+  "225": "Temp. occupied w/persons with ure",
+  "226": "Vacant regular",
+  "227": "Vacant - storage of hhld furniture",
+  "228": "Unfit, to be demolished",
+  "229": "Under construction, not ready",
+  "230": "Converted to temp business or storage",
+  "231": "Unoccupied tent or trailer site",
+  "232": "Permit granted - construction not started",
+  "233": "Other - specify",
+  "240": "Demolished",
+  "241": "House or trailer moved",
+  "242": "Outside segment",
+  "243": "Converted to perm. business or storage",
+  "244": "Merged",
+  "245": "Condemned",
+  "246": "Built after April 1, 2000",
+  "247": "Unused serial no./listing sheet line",
+  "248": "Other - specify",
+  "256": "Removed during sub-sampling",
+  "257": "Unit already had a chance of selection"}
+
+housing_map = {
+  " 0": "Other unit",
+  " 1": "House, apartment, flat",
+  " 2": "HU in nontransient hotel, motel, etc.",
+  " 3": "HU permanent in transient hotel, motel",
+  " 4": "HU in rooming house",
+  " 5": "Mobile home or trailer w/no perm. room added",
+  " 6": "Mobile home or trailer w/1 or more perm. rooms added",
+  " 7": "HU not specified above",
+  " 8": "Quarters not HU in rooming or brding hs",
+  " 9": "Unit not perm. in transient hotl, motl",
+  "10": "Unoccupied tent site or trlr site",
+  "11": "Student quarters in college dorm",
+  "12": "Other unit not specified above"
+}
+
+housing_t_map = {
+  " 0": "Non-interview household",
+  " 1": "Husband/wife primary family (neither AF)",
+  " 2": "Husb/wife prim. family (either/both AF)",
+  " 3": "Unmarried civilian male-prim. fam hhlder",
+  " 4": "Unmarried civ. female-prim fam hhlder",
+  " 5": "Primary family hhlder-rp in AF, unmar.",
+  " 6": "Civilian male primary individual",
+  " 7": "Civilian female primary individual",
+  " 8": "Primary individual hhld-rp in AF",
+  " 9": "Group quarters with family",
+  "10": "Group quarters without family"
+}
+
+yes_no_map = {
+  " 1": "Yes",
+  " 2": "No"
+}
+
+interview_map = {
+  " 0": "Noninterview/Indeterminate",
+  " 1": "Personal",
+  " 2": "Telephone"
+}
+
+income_map = {
+  " 1": "Less than $5,000",
+  " 2": "$5,000 to $7,499",
+  " 3": "$7,500 to $9,999",
+  " 4": "$10,000 to $12,499",
+  " 5": "$12,500 to $14,999",
+  " 6": "$15,000 to $19,999",
+  " 7": "$20,000 to $24,999",
+  " 8": "$25,000 to $29,999",
+  " 9": "$30,000 to $34,999",
+  "10": "$35,000 to $39,999",
+  "11": "$40,000 to $49,999",
+  "12": "$50,000 to $59,999",
+  "13": "$60,000 to $74,999",
+  "14": "$75,000 to $99,999",
+  "15": "$100,000 to $149,999",
+  "16": "$150,000 or more"
+}
+
+geography_map = {
+  " 1": "Northeast",
+  " 2": "Midwest (formerly North Central)",
+  " 3": "South",
+  " 4": "West"
+}
+
+race_map = {
+  " 1": "White Only",
+  " 2": "Black Only",
+  " 3": "American Indian, Alaskan Native Only",
+  " 4": "Asian Only",
+  " 5": "Hawaiian/Pacific Islander Only",
+  " 6": "White-Black",
+  " 7": "White-AI",
+  " 8": "White-Asian",
+  " 9": "White-HP",
+  "10": "Black-AI",
+  "11": "Black-Asian",
+  "12": "Black-HP",
+  "13": "AI-Asian"
+}
+
